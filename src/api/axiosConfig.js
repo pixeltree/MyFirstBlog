a0af3dda3dbc564005be53cd95d0a852970f3e3d
Max Halleran <maxhalleran@gmail.com>
2022-11-24 21:44:49 -0700

    Reworked axios config to work without valid url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,27 @@
 import axios from "axios"
 
-const baseUrl = process.env.SERVER_URL || 'https://localhost:5001'
+/**
+ * Returns true if input string is a valid url.
+ * Returns false other wise
+ * @param input
+ * @returns {boolean}
+ */
+export const isHyperlink = (input) => {
+  try {
+    return !! new URL(input, input)
+  } catch (_) {
+    return false
+  }
+}
 
-const JSON_CLIENT = axios.create({
-  baseURL: baseUrl,
-  timeout: 3000,
-  rejectUnauthorized: false,
-})
+const BASE_URL = process.env.SERVER_URL || 'https://localhost:5001'
+
+const AXIOS_BASE = axios.create({
+    baseURL: baseUrl,
+    timeout: 3000,
+    rejectUnauthorized: false,
+  })
+
+const JSON_CLIENT = isHyperlink(BASE_URL) ? AXIOS_BASE : false
 
 export default JSON_CLIENT
